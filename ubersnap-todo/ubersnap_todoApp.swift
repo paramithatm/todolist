d17616720ba090e6a2648614901be43b88076248
Paramitha <paramithatm@gmail.com>
2023-08-08 16:07:52 +0700

    Initial Commit

---
+++ ubersnap-todo/ubersnap_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ubersnap_todoApp.swift
+//  ubersnap-todo
+//
+//  Created by Paramitha on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ubersnap_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
