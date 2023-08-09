bea2beaffa0d8595fd8487ea897b9793072af46c
Paramitha <paramithatm@gmail.com>
2023-08-09 21:18:19 +0700

    rename model because of typo

--- ubersnap-todo/CoreDataController.swift
+++ ubersnap-todo/CoreDataController.swift
@@ -9,32 +9,12 @@ import CoreData
 import Foundation
 
 struct CoreDataController {
-    // A singleton for our entire app to use
     static let shared = CoreDataController()
 
-    // Storage for Core Data
     let container: NSPersistentContainer
 
-    // A test configuration for SwiftUI previews
-    static var preview: CoreDataController = {
-        let controller = CoreDataController()
-
-        // Create 10 example programming languages.
-        for _ in 0..<5 {
-            let testData = Todo(context: controller.container.viewContext)
-            testData.title = "Laundry"
-            testData.desc = "One laundry on Wednesday"
-            testData.dueDate = Date()
-        }
-
-        return controller
-    }()
-
-    // An initializer to load Core Data
     init() {
-        // If you didn't name your model Main you'll need
-        // to change this name below.
-        container = NSPersistentContainer(name: "ubsersnap-todo")
+        container = NSPersistentContainer(name: "ubersnap-todo")
 
         container.loadPersistentStores { description, error in
             if let error = error {
